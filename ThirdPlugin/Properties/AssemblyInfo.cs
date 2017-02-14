f557b05eee7dfa53ef4416b7bad3b49f37f1449f
Alex Fielder <alex.fielder@gmail.com>
2017-02-14 17:07:50 +0000

    Add project files.

---
+++ ThirdPlugin/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ThirdPlugin")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard Company")]
+[assembly: AssemblyProduct("ThirdPlugin")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard Company 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f8ce92c7-cb8b-46a4-ae3e-f04e8869a13f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ThirdPlugin/ThirdPlugin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using PluginContracts;
+
+namespace ThirdPlugin
+{
+    public class ThirdPlugin : IPlugin
+    {
+        public string Name
+        {
+            get
+            {
+                return "Third Plugin";
+            }
+        }
+
+        public void Do()
+        {
+            System.Windows.MessageBox.Show("Do Something in Third Plugin");
+        }
+    }
+}
