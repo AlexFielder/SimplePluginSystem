f7ee12a3a614d8cf5fbe7cd17f5f9056913724f9
Alex Fielder <alex.fielder@gmail.com>
2017-02-14 17:33:54 +0000

    Added Fourth Plugin to prove the steps required.

---
+++ FourthPlugin/FourthPlugin.cs
@@ -0,0 +1,25 @@
+﻿using PluginContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FourthPlugin
+{
+    public class FourthPlugin : IPlugin
+    {
+        public string Name
+        {
+            get
+            {
+                return "Fourth Plugin";
+            }
+        }
+
+        public void Do()
+        {
+            System.Windows.MessageBox.Show("The Fourth Plugin");
+        }
+    }
+}
